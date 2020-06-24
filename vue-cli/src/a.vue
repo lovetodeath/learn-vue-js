49b0b75d6bee7ab08671d84cebd461c4af836126
이준한 <jhlee83@interpark.com>
2020-06-24 18:14:25 +0900

    템플릿, Vue.js Cli, 싱글 파일 컴포넌트

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        {{ str }}
+        <app-header
+                v-bind:propsdata="str"
+                v-on:renew="renewStr"
+        ></app-header>
+    </div>
+</template>
+
+<script>
+    import AppHeader from './components/AppHeader.vue';
+
+    export default {
+        data: function () {
+            return {
+                str: "hi"
+            }
+        },
+        components: {
+            'app-header': AppHeader
+        },
+        methods: {
+            renewStr: function () {
+                this.str = 'hello';
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
